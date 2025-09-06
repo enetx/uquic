e3e00fb5184287587c6b683a3f71ae11ee560a92
enetx <163449931+enetx@users.noreply.github.com>
2025-09-06 11:25:12 +0200

    first commit

---
+++ example/uquic/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"github.com/enetx/http"
+	"os"
+
+	tls "github.com/refraction-networking/utls"
+
+	quic "github.com/enetx/uquic"
+	"github.com/enetx/uquic/http3"
+)
+
+func main() {
+	keyLogWriter, err := os.Create("./keylog.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	tlsConf := &tls.Config{
+		// ServerName: "quic.tlsfingerprint.io",
+		// ServerName: "www.cloudflare.com",
+		// MinVersion:   tls.VersionTLS13,
+		KeyLogWriter: keyLogWriter,
+		// NextProtos:   []string{"h3"},
+	}
+
+	quicConf := &quic.Config{}
+
+	roundTripper := &http3.RoundTripper{
+		TLSClientConfig: tlsConf,
+		QuicConfig:      quicConf,
+	}
+
+	quicSpec, err := quic.QUICID2Spec(quic.QUICFirefox_116)
+	// quicSpec, err := quic.QUICID2Spec(quic.QUICChrome_115)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	uRoundTripper := http3.GetURoundTripper(
+		roundTripper,
+		&quicSpec,
+		// getCRQUICSpec(),
+		nil,
+	)
+	defer uRoundTripper.Close()
+
+	hclient := &http.Client{
+		Transport: uRoundTripper,
+	}
+
+	addr := "https://quic.tlsfingerprint.io/qfp/?beautify=true"
+	// addr := "https://www.cloudflare.com"
+
+	rsp, err := hclient.Get(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Got response for %s: %#v", addr, rsp)
+
+	body := &bytes.Buffer{}
+	_, err = io.Copy(body, rsp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Response Body: %s", body.Bytes())
+}
---
+++ integrationtests/gomodvendor/go.mod
@@ -0,0 +1,29 @@
+module test
+
+go 1.21
+
+require github.com/enetx/uquic v0.0.5
+
+require (
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/gaukas/clienthellod v0.4.2 // indirect
+	github.com/gaukas/godicttls v0.0.4 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/google/pprof v0.0.0-20231023181126-ff6d637d2a7b // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/onsi/ginkgo/v2 v2.13.0 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/refraction-networking/utls v1.6.4 // indirect
+	go.uber.org/mock v0.4.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
+)
+
+replace github.com/quic-go/quic-go => ../../
